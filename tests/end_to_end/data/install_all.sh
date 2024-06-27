be097723cb7de9693f622573db59343129937724
Matthew Hughes <matthewhughes934@gmail.com>
2024-06-27 20:03:11 +0100

    Move to GitHub actions
    Because it's simpler to configure than CircleCI (at least for me, I
    haven't used CircleCI for a while now so there may be some tricks I've
    missed)

--- tests/end_to_end/data/install_all.sh
+++ tests/end_to_end/data/install_all.sh
@@ -7,7 +7,7 @@ HERE="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 && pwd -P)"
 find "$HERE/deps" -maxdepth 1 -mindepth 1 -type d | while read -r dep_dir
 do
     cd -- "$dep_dir"
-    pip install --quiet .
+    pip install --disable-pip-version-check --quiet .
 done
 
 find "$HERE" -maxdepth 1 -mindepth 1 -type d -name "test_pkg_*" | while read -r pkg_dir
